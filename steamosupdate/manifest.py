539cba50dcd02af0e7234c40fefbb97069407cc4
Arnaud Rebillout <arnaud.rebillout@collabora.com>
2018-11-30 16:23:26 +0700

    Rework mkmanifest
    Signed-off-by: Arnaud Rebillout <arnaud.rebillout@collabora.com>

--- steamosupdate/manifest.py
+++ steamosupdate/manifest.py
@@ -17,11 +17,11 @@
 # <http://www.gnu.org/licenses/>.
 
 from collections import namedtuple
-import distro
 import json
 import platform
+import re
 
-import steamosupdate.version as version
+import steamosupdate.version as ver
 
 Manifest = namedtuple(
     'Manifest',
@@ -31,7 +31,7 @@ def make_from_data(data):
 
     """Create a Manifest from a dictionary.
 
-    Raise exceptions if the dictionary doesn't contains the
+    Raise exceptions if the dictionary doesn't contain the
     expected keys.
     """
 
@@ -66,35 +66,50 @@ def make_from_file(filename):
     # Make a manifest from data, might raise exceptions
     return make_from_data(data)
 
-def make_from_running_os():
+def _load_os_release():
 
-    """Create a Manifest from the current running system.
+    """Load /etc/os-release in a dictionary
     """
 
-    product = distro.id()
-    release = distro.codename()
+    envre = re.compile(r'''^([^\s=]+)=(?:[\s"']*)(.+?)(?:[\s"']*)$''')
+    result = {}
 
+    with open('/etc/os-release') as f:
+        for line in f:
+            match = envre.match(line)
+            if match is not None:
+                result[match.group(1)] = match.group(2)
+
+    return result
+
+def make_from_running_os(release, variant, version, checkpoint):
+
+    """Create a Manifest from the current running system.
+
+    If arguments are None, they're looked for in the os-release file,
+    except for 'checkpoint', which does not exist in any standard place.
+    Note that os-release allows custom additional fields (and recommends
+    to use some namespacing), so we could look for 'checkpoint' in a
+    custom field named ${PRODUCT}_CHECKPOINT, for example.
+    """
+
+    # All these fields have a fallback value in the os-release
+    osrel = _load_os_release()
+    product = osrel['ID']
+    if not release:
+        release = osrel['VERSION_CODENAME']
+    if not variant:
+        variant = osrel['VARIANT_ID']
+    if not version:
+        version = osrel['VERSION_ID']
+
+    # Arch comes from the platform, with a twist
     arch = platform.machine()
     if arch == 'x86_64':
         arch = 'amd64'
 
-    variant = distro.os_release_attr('variant_id')
-
-    # To handle the version, we give precedence to the file
-    # /etc/$PRODUCT_version if it exists, otherwise we use
-    # the function distro.version().
-    version_file = '/etc/' + product + '_version'
-    try:
-        with open(version_file, 'r') as f:
-            version = f.read()
-    except OSError:
-        version = distro.version()
-
-    # Ensure we understand the version
-    version.parse_string(version, 'guess')
-
-    # We have no idea whether we're a checkpoint release
-    checkpoint = False
+    # Make sure we understand the version
+    ver.parse_string(version, 'guess')
 
     manifest = Manifest(product, release, arch, variant, version, checkpoint)
 
@@ -102,7 +117,7 @@ def make_from_running_os():
 
 def write_to_string(manifest, **kwargs):
 
-    """Write a Manifest to file.
+    """Write a Manifest to a string.
     """
 
     data = manifest._asdict()
