72202c9f1e2da3984aa2dfa9e7fdc16b4bbcf163
Ludovico de Nittis <ludovico.denittis@collabora.com>
2022-05-11 14:06:27 +0200

    build-image-hierarchy: Add default values to reduce boilerplate
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- examples/build-image-hierarchy.sh
+++ examples/build-image-hierarchy.sh
@@ -40,13 +40,13 @@ EOF
 
 fake_image() {
 
-    local product=$1
-    local release=$2
-    local variant=$3
-    local arch=$4
-    local version=$5
-    local buildid=$6
-    local checkpoint=$7
+    local variant=$1
+    local version=$2
+    local buildid=$3
+    local checkpoint=$4
+    local product=${5:-steamos}
+    local release=${6:-holo}
+    local arch=${7:-amd64}
 
     local imgdir imgname
 
@@ -71,17 +71,17 @@ mkdir -p snapshots
 (
   cd snapshots
 
-  fake_image steamos holo steamdeck amd64 snapshot 20181102.1 0
-  fake_image steamos holo steamdeck-beta amd64 snapshot 20181102.1 0
-  fake_image steamos holo steamdeck-rc amd64 snapshot 20220215.0 0
-  fake_image steamos holo atomic amd64 snapshot 20181102.1 0
+  fake_image steamdeck snapshot 20181102.1 0
+  fake_image steamdeck-beta snapshot 20181102.1 0
+  fake_image steamdeck-rc snapshot 20220215.0 0
+  fake_image atomic snapshot 20181102.1 0
 
-  fake_image steamos holo steamdeck amd64 snapshot 20181102.2 0
-  fake_image steamos holo atomic amd64 snapshot 20181102.2 0
+  fake_image steamdeck snapshot 20181102.2 0
+  fake_image atomic snapshot 20181102.2 0
 
-  fake_image steamos holo steamdeck amd64 snapshot 20181108.1 0
-  fake_image steamos holo steamdeck-beta amd64 snapshot 20181108.1 0
-  fake_image steamos holo atomic amd64 snapshot 20181108.1 0
+  fake_image steamdeck snapshot 20181108.1 0
+  fake_image steamdeck-beta snapshot 20181108.1 0
+  fake_image atomic snapshot 20181108.1 0
 )
 
 mkdir -p releases
@@ -89,21 +89,21 @@ mkdir -p releases
 (
   cd releases
 
-  fake_image steamos holo steamdeck amd64 3.1 20220401.1 0
-  fake_image steamos holo steamdeck amd64 3.1 20220402.3 1
-  fake_image steamos holo steamdeck amd64 3.2 20220411.1 0
-  fake_image steamos holo steamdeck amd64 3.3 20220423.1 1
+  fake_image steamdeck 3.1 20220401.1 0
+  fake_image steamdeck 3.1 20220402.3 1
+  fake_image steamdeck 3.2 20220411.1 0
+  fake_image steamdeck 3.3 20220423.1 1
 
-  fake_image steamos holo steamdeck-rc amd64 3.1 20220401.5 0
+  fake_image steamdeck-rc 3.1 20220401.5 0
 
   # Same checkpoint as in 'steamdeck'
-  fake_image steamos holo steamdeck-beta amd64 3.1 20220402.3 1
-  fake_image steamos holo steamdeck-beta amd64 3.1 20220405.100 0
+  fake_image steamdeck-beta 3.1 20220402.3 1
+  fake_image steamdeck-beta 3.1 20220405.100 0
   # The buildid of a checkpoint can be different, as long as they are
   # conceptually the same (i.e. introduce the same breaking changes)
-  fake_image steamos holo steamdeck-beta amd64 3.3 20220423.100 1
+  fake_image steamdeck-beta 3.3 20220423.100 1
   # Testing a new checkpoint that still hasn't been promoted to 'steamdeck'
-  fake_image steamos holo steamdeck-beta amd64 3.4 20220501.100 1
+  fake_image steamdeck-beta 3.4 20220501.100 1
 )
 
 mkdir -p releases-and-snaps
@@ -111,17 +111,17 @@ mkdir -p releases-and-snaps
 (
   cd releases-and-snaps
 
-  fake_image steamos holo steamdeck amd64 snapshot 20220201.1 0
-  fake_image steamos holo steamdeck amd64 snapshot 20220225.1 0
-  fake_image steamos holo steamdeck amd64 3.0 20220303.2 0
+  fake_image steamdeck snapshot 20220201.1 0
+  fake_image steamdeck snapshot 20220225.1 0
+  fake_image steamdeck 3.0 20220303.2 0
 
-  fake_image steamos holo steamdeck-rc amd64 3.0 20220303.1 0
+  fake_image steamdeck-rc 3.0 20220303.1 0
 
-  fake_image steamos holo steamdeck-beta amd64 snapshot 20220221.100 0
-  fake_image steamos holo steamdeck-beta amd64 3.1 20220301.100 0
+  fake_image steamdeck-beta snapshot 20220221.100 0
+  fake_image steamdeck-beta 3.1 20220301.100 0
 
-  fake_image steamos holo steamdeck-main amd64 3.1 20220302.1005 0
-  fake_image steamos holo steamdeck-main amd64 3.2 20220304.1000 0
+  fake_image steamdeck-main 3.1 20220302.1005 0
+  fake_image steamdeck-main 3.2 20220304.1000 0
 )
 
 echo "Hierarchy created under '$OUTDIR/images'"
