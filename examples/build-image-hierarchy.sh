348b3ca4eb0cb9ff3fb22b3e30f4c9b053c6c4d8
Ludovico de Nittis <ludovico.denittis@collabora.com>
2023-10-31 15:58:28 +0100

    tests: Remove checkpoint for snapshot
    In Jupiter we already moved to versioned images, so in the real world it
    can't be possible to have a checkpoint for a snapshot.
    
    Remove that test because it is not a realistic scenario.
    
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- examples/build-image-hierarchy.sh
+++ examples/build-image-hierarchy.sh
@@ -82,14 +82,12 @@ mkdir -p releases-and-snaps2
 (
   cd releases-and-snaps2
 
-  # Simulate the case where we had to set up a checkpoint before the
-  # switch to versioned images was completed
   fake_image steamdeck snapshot 20230201.1 false
-  fake_image steamdeck snapshot 20230303.1 true
+  fake_image steamdeck 3.5 20230303.1 false
   fake_image steamdeck 3.5 20230401.1 true
   fake_image steamdeck 3.5 20230411.1 false
 
-  fake_image steamdeck-beta snapshot 20230303.100 true
+  fake_image steamdeck-beta snapshot 20230303.100 false
   fake_image steamdeck-beta 3.5 20230401.100 true
   fake_image steamdeck-beta 3.6 20230727.100 false
 )
